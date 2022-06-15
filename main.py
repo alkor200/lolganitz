3dd72de98fb7cf5028395076e2417e2f1cfdb218
Felix Nirsberger <felix@nirsberger.de>
2022-06-15 17:59:18 +0200

    adddddssss

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ relais = [26, 16, 20, 21, 5, 6, 13, 19, 17, 18, 27, 22, 23, 24, 25, 12]
 def init():
     for pin in relais:
         GPIO.setup(pin, GPIO.OUT)
-
+        GPIO.output(pin, GPIO.LOW)
 
 def switch_on(relais_pin, on_time):
-    GPIO.output(relais_pin, GPIO.HIGH)
-    time.sleep(on_time)
-    GPIO.output(relais_pin, GPIO.LOW)
+    try:
+        GPIO.output(relais_pin, GPIO.HIGH)
+        time.sleep(on_time)
+        GPIO.output(relais_pin, GPIO.LOW)
+    except KeyboardInterrupt:
+        GPIO.output(relais_pin, GPIO.LOW)
 
 
 if __name__ == '__main__':
     init()
 
     for pin in relais:
-        switch_on(pin, 1)
-
+        switch_on(pin, 0.1)
