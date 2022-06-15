ce4e2b4b40a5fbf62b04a810518ddd2e3ad33802
Felix Nirsberger <felix@nirsberger.de>
2022-06-15 17:44:25 +0200

    first test script

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import time
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+relais = [26, 16, 20, 21, 5, 6, 13, 19, 17, 18, 27, 22, 23, 24, 25, 12]
+
+def init():
+    for pin in relais:
+        GPIO.setup(pin, GPIO.OUT)
+
+
+def switch_on(relais_pin, on_time):
+    GPIO.output(relais_pin, GPIO.HIGH)
+    time.sleep(on_time)
+    GPIO.output(relais_pin, GPIO.LOW)
+
+
 if __name__ == '__main__':
-    pass
+    init()
+
+    for pin in relais:
+        switch_on(pin, 1)
+
