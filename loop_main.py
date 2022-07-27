67ba4cac38068bdd3b1cbc4fbe7126da796c1372
Felix Nirsberger <felix@nirsberger.de>
2022-07-27 23:55:25 +0200

    changes

--- loop_main.py
+++ loop_main.py
@@ -248,7 +248,7 @@ class LightManager:
 
         for light in off_lights:
             light.turn_off()
-        time.sleep(0.05)
+        time.sleep(0.1)
 
     def every_second_light(self):
         second = False
